a635e6719e731b6ccd7b4cb6204458aa87c5bf9e
unknown <neilt1111@yahoo.com>
2016-02-19 16:46:02 -0500

    Added scoring system, set of 5

--- TronFrame.java
+++ TronFrame.java
@@ -36,8 +36,9 @@ public class TronFrame extends JFrame implements KeyListener//, ActionListener
 {
 	public static void main(String[] args) throws Exception {
 		TronFrame gameFrame = new TronFrame();
-		gameFrame.startAnimation(75);
-		gameFrame.playGame();
+		gameFrame.playSet();
+		//gameFrame.startAnimation();
+		//gameFrame.playGame();
 	}
 
 	public int[][] cellstates;
@@ -57,13 +58,14 @@ public class TronFrame extends JFrame implements KeyListener//, ActionListener
 	public static final Color deathColor = new Color(200,0,0);
 	private TronPanel tp;
 
-	private int[] score;// = new int[5];
+	private int[] scores;// = new int[5];
 
 	// starting position:
 	// blue: 10,20
 	// orange: 30,20
 	public TronFrame() {
 		super("Tron Test");
+		startStuff();
 		addKeyListener(this);
 		this.setResizable(false);
 		this.setSize(400,400); // somewhat irrelevant
@@ -73,24 +75,37 @@ public class TronFrame extends JFrame implements KeyListener//, ActionListener
 		tp = new TronPanel();
 		contentPane.add(tp);
 		this.pack();
-		blueCycle = new TronCycle(10,20,2,1,4);
-		orangeCycle = new TronCycle(30,20,4,2,5);
-		cellstates = new int[41][41];
-		score = new int[5];
+		scores = new int[5];
 		for(int i = 0; i < 5; i ++) {
-			score[i] = -1;
+			scores[i] = -1;
 		}
-		score[3] = 2;
-		score[1] = 1;
+		//scores[3] = 2;
+		//scores[1] = 1;
 		// cellstates: x, y
 	}
 
-	public void playGame() throws Exception {
-//		playGame(25);
-		playGame(75);
+	public void playSet() throws Exception {
+		int winner;
+		for(int j = 0; j < 5; j ++) {
+			winner = 3;
+			while(winner == 3) {
+				startStuff();
+				startAnimation();
+				winner = playGame();
+				Thread.sleep(1000);
+			}
+			scores[j] = winner;
+			this.repaint();
+			Thread.sleep(250);
+		}
 	}
 
-	public void playGame(int tickspeed) throws Exception {
+	public int playGame() throws Exception {
+//		playGame(25);
+		return playGame(75);
+	}
+
+	public int playGame(int tickspeed) throws Exception {
 		//this.act();
 		while(blueCycle.isAlive && orangeCycle.isAlive) {
 			Thread.sleep(tickspeed);
@@ -98,25 +113,47 @@ public class TronFrame extends JFrame implements KeyListener//, ActionListener
 		}
 		if(blueCycle.isAlive) {
 			System.out.println("Blue Cycle wins!");
+			return 1;
 		}
 		else if(orangeCycle.isAlive) {
 			System.out.println("Orange Cycle wins!");
+			return 2;
 		}
 		else {
 			System.out.println("It's a tie!");
+			return 3;
 		}
+	}
+
+	public void startStuff() {
+		blueCycle = new TronCycle(10,20,2,1,4);
+		orangeCycle = new TronCycle(30,20,4,2,5);
+		cellstates = new int[41][41];
+		/*for(int i = 0; i < 41; i ++) {
+			for(int j = 0; j < 41; j ++) {
+				cellstates[i][j] = 0;
+			}
+		}*/
+	}
+
+	public void startAnimation() throws Exception {
+		startAnimation(75);
 	}
 
 	public void startAnimation(int tickspeed) throws Exception {
 		for(int i = 0; i < 3; i ++) {
-			cellstates[10][20] = 1;
-			cellstates[30][20] = 2;
+			cellstates[10][20] = 4;
+			cellstates[30][20] = 5;
 			this.repaint();
+			orangeCycle.hasChangedDir = false;
+			blueCycle.hasChangedDir = false;
 			Thread.sleep(5*tickspeed);
 			cellstates[10][20] = 0;
 			cellstates[30][20] = 0;
 			this.repaint();
 			Thread.sleep(2*tickspeed);
+			orangeCycle.hasChangedDir = false;
+			blueCycle.hasChangedDir = false;
 		}
 	}
 
@@ -205,6 +242,7 @@ public class TronFrame extends JFrame implements KeyListener//, ActionListener
 		public int Y;
 		private int lX;
 		private int lY;
+		private int initDir;
 		public int dir;
 		public int color;
 		public int scolor;
@@ -215,10 +253,11 @@ public class TronFrame extends JFrame implements KeyListener//, ActionListener
 		 *     4-+-2
 		 *       3
 		 */
-		public TronCycle(int initX, int initY, int initDir, int rColor, int tColor) {
+		public TronCycle(int initX, int initY, int iDir, int rColor, int tColor) {
 			X = initX;
 			Y = initY;
-			dir = initDir;
+			dir = 5;
+			initDir = iDir;
 			color = rColor;
 			scolor = tColor;
 			hasChangedDir = false;
@@ -246,6 +285,9 @@ public class TronFrame extends JFrame implements KeyListener//, ActionListener
 		}
 
 		public void move(int[][] cells) {
+			if(dir == 5) {
+				dir = initDir;
+			}
 			cells[X][Y] = color;
 			lX = X;
 			lY = Y;
@@ -262,6 +304,7 @@ public class TronFrame extends JFrame implements KeyListener//, ActionListener
 				}
 			}
 			else { // out of bounds
+				cells[lX][lY] = TronFrame.DEATH_COLOR;
 				isAlive = false;
 			}
 			hasChangedDir = false;
@@ -281,10 +324,10 @@ public class TronFrame extends JFrame implements KeyListener//, ActionListener
 			super.paintComponent(g);
 			g.setColor(new Color(70,70,70));
 			g.fillRect(0,0,472,502);
-			g.setColor(new Color(35,35,35));
+			//g.setColor(new Color(35,35,35));
 			//g.fillRect(3,170,24,132);
-			g.fillRect(170,18,132,24);
 			g.setColor(new Color(0,0,0));
+			g.fillRect(170,16,132,28);
 			g.fillRect(30,60,412,412);
 			g.setColor(new Color(20,20,70));
 			for(int i = 0; i < 42; i ++) {
@@ -292,6 +335,34 @@ public class TronFrame extends JFrame implements KeyListener//, ActionListener
 				g.drawLine(30,60+10*i+1,441,60+10*i+1);
 				g.drawLine(30+10*i,60,30+10*i,471);
 				g.drawLine(30+10*i+1,60,30+10*i+1,471);
+			}
+			// these lines go 1 over where they should (???)
+			g.drawLine(170,14,302,14);
+			g.drawLine(170,15,302,15);
+			g.drawLine(170,16,302,16);
+			g.drawLine(170,17,302,17);
+			g.drawLine(170,42,302,42);
+			g.drawLine(170,43,302,43);
+			g.drawLine(170,44,302,44);
+			g.drawLine(170,45,302,45);
+
+			g.drawLine(169,14,169,45);
+			g.drawLine(168,14,168,45);
+			g.drawLine(302,14,302,45);
+			g.drawLine(303,14,303,45);
+			for(int i = 0; i <= 5; i ++) {
+				g.drawLine(170+26*i,14,170+26*i,45);
+				g.drawLine(170+26*i+1,14,170+26*i+1,45);
+			}
+			for(int i = 0; i < 5; i ++) {
+				if(scores[i] == 1) {
+					g.setColor(blueColor);
+					g.fillRect(172+26*i,18,24,24);
+				}
+				else if(scores[i] == 2) {
+					g.setColor(orangeColor);
+					g.fillRect(172+26*i,18,24,24);
+				}
 			}
 			int tmp;
 			for(int i = 0; i < 41; i ++) {
